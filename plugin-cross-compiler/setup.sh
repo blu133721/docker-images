af118683d17b25a7a553b0c5f23ebdffd7307a4a
blu <73272097+blu133721@users.noreply.github.com>
2024-03-23 11:40:50 +0200

    Update setup.sh

--- plugin-cross-compiler/setup.sh
+++ plugin-cross-compiler/setup.sh
@@ -2,7 +2,13 @@
 
 cd tempfolder
 git clone https://github.com/swiftly-solution/swiftly
-cd swiftly; mv plugin_files/scripting/* ..; cd ..
+cd swiftly
+
+# Checkout the specific commit
+git checkout cd713350d4987aa081650fe6d067ed790b24fd77
+
+# Move the files
+mv plugin_files/scripting/* ..; cd ..
 
 cd /tempfolder/${1}
 make
